ab1e8b8696cac55d836d2241d0a7751822432910
Laura <lauramhfur@gmail.com>
2023-10-09 14:59:43 +0200

    Update test_kmer.py

--- src/test_kmer.py
+++ src/test_kmer.py
@@ -2,5 +2,17 @@
 # all files that start with test_*.py and run all functions with
 # names that start with test_
 
-def test_me():
-    assert False, "Don't forget to write tests"
+from kmer import (
+    kmer,
+    unique_kmers,
+    count_kmers
+)
+
+def test_kmer():
+    assert kmer("agtagtcg", 3) == ['agt', 'gta', 'tag', 'agt', 'gtc', 'tcg']
+
+def test_unique_kmers():
+    assert unique_kmers("agtagtcg", 3) == ['agt', 'gta', 'tag', 'gtc', 'tcg']
+
+def test_count_kmers():
+    assert count_kmers("agtagtcg", 3) == {'agt': 2, 'gta': 1, 'tag': 1, 'gtc': 1, 'tcg': 1}
